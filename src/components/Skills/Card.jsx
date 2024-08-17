1ce220634f0400d89e60e146fa539274a2ee57dd
Yuva Sri Sai <nanithota18102004@gmail.com>
2024-08-18 01:44:58 +0530

    mini

--- src/components/Skills/Card.jsx
+++ src/components/Skills/Card.jsx
@@ -3,7 +3,14 @@ import { Box, Tooltip, IconButton } from "@mui/material";
 import { Link } from "react-router-dom";
 export default function Card({ obj }) {
   return (
-    <Box>
+    <Box
+      sx={{
+          transition: "transform 0.3s ease-in-out", // Smooth transition effect
+          "&:hover": {
+            transform: "scale(1.2)", // Scale up the image by 1.2 times
+          },
+        }}
+    >
       <Link to={obj.link || "#"} target="_blank">
         <Tooltip title={obj.name} placement="top">
           <Box
